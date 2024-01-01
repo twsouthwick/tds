1604ba0167c91c248b62266274a57b961c0f0e07
Taylor Southwick <tasou@microsoft.com>
2024-01-01 10:50:36 -0800

    remove unused

--- data/update.ps1
+++
@@ -1,3 +0,0 @@
-$openssl = "C:\Program Files\Git\usr\bin\openssl.exe"
-
-& $openssl req -x509 -nodes -newkey rsa:2048 -subj '/CN=sql.docker.internal' -keyout "$PSScriptRoot/mssql.key" -out "$PSScriptRoot/mssql.pem" -days 365
